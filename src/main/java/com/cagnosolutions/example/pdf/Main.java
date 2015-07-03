fa3a0d3ec9222853f9eb929ed243188cb27543ab
scottcagno <scottiecagno@gmail.com>
2015-07-02 20:37:01 -0400

    PDFDocumentFormFillExample[master] -- first

---
+++ src/main/java/com/cagnosolutions/example/pdf/Main.java
@@ -0,0 +1,42 @@
+package main.java.com.cagnosolutions.example.pdf;
+
+import org.apache.pdfbox.exceptions.COSVisitorException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Scott Cagno.
+ * Copyright Cagno Solutions. All rights reserved.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+
+			// setup pdf documents
+			String pdfForm = "blankPdfForm.pdf";
+			String pdfFill = "filledPdfForm.pdf";
+
+			// map we will use to fill out the pdf, key / value style
+			Map<String,String> data = new HashMap<>();
+			data.put("fullName", "Scott Cagno");
+			data.put("company", "Cagno Solutions, LLC");
+
+			// instantiate pdf toolkit class
+			PDFToolkit toolkit = new PDFToolkit();
+
+			try {
+				toolkit.populateAndCopy(originalPdf, targetPdf, data);
+			} catch (IOException | COSVisitorException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println("Complete");
+
+		}
+	}
+
+}
